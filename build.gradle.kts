891e4f832a9f345cda167d1449777c8a8360e865
xDaoud <moshdawd@gmail.com>
2026-07-29 00:01:28 +0300

    chore: initialise Gradle project

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass = "dev.xDaoud.miniredis.Main"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
